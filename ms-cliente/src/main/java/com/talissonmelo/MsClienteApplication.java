dbed03d92eb5d17790a55c61be024e75b06e2848
TalissonMelo <talissonmelorodrigues@gmail.com>
2022-07-20 21:17:31 -0300

    Registrando o Microserviço de CLIENTE no Servidor EUREKA

--- ms-cliente/src/main/java/com/talissonmelo/MsClienteApplication.java
+++ ms-cliente/src/main/java/com/talissonmelo/MsClienteApplication.java
@@ -2,8 +2,10 @@ package com.talissonmelo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsClienteApplication {
 
 	public static void main(String[] args) {
