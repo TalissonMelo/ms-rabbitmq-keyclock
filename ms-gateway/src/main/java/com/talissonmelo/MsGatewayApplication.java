9d2429e179e045f2c074127aa31774eb659acdce
TalissonMelo <talissonmelorodrigues@gmail.com>
2022-07-22 21:04:29 -0300

    Criado o Cloud Gateway

---
+++ ms-gateway/src/main/java/com/talissonmelo/MsGatewayApplication.java
@@ -0,0 +1,27 @@
+package com.talissonmelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class MsGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder routeLocatorBuilder) {
+		return  routeLocatorBuilder.routes()
+				.route( r -> r.path("/clientes/**").uri("lb://ms-cliente"))
+				.build();
+	}
+
+}
