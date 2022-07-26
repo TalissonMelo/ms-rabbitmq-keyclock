59e7b1994c001c537d8925f240b12b2b3caa1795
TalissonMelo <talissonmelorodrigues@gmail.com>
2022-07-26 20:54:25 -0300

    Criado endPoint para busca de cartões com critério renda

--- ms-cartao/src/main/java/com/talissonmelo/aplicacao/CartaoControlador.java
+++ ms-cartao/src/main/java/com/talissonmelo/aplicacao/CartaoControlador.java
@@ -5,10 +5,9 @@ import com.talissonmelo.modelo.dto.CartaoInsercao;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/cartoes")
@@ -27,5 +26,11 @@ public class CartaoControlador {
         Cartao cartao = new Cartao(insercao);
         servico.salvar(cartao);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping(params = "renda")
+    public ResponseEntity<List<Cartao>> getCartoesRendaAte(@RequestParam("renda") Long renda) {
+        List<Cartao> cartoes = servico.buscarCartaoRendaMenorIgual(renda);
+        return ResponseEntity.ok().body(cartoes);
     }
 }
